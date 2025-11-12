e2f5d3a9f89ed1b720bf7e0ef5e630db41a6311d
Divya Pratap Singh <38037100+divu10@users.noreply.github.com>
2025-11-12 14:45:37 +0530

    Create server.js

---
+++ examples/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const next = require('next');
+const port = process.env.PORT || 3000;
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  http.createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, () => console.log(`Server listening on ${port}`));
+});
